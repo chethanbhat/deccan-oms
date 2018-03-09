c6a3482c01a38f462ceb8351999f6e5d8179a2bd
Chethan Bhat <chethanbhat@gmail.com>
2018-03-09 19:44:33 +0530

    Complete CRUD for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
 const User = require('../models/user');
+const Order = require('../models/order');
+const async = require('async');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // Display list of all Users
 exports.user_list = (req, res) => {
@@ -26,30 +30,151 @@ exports.user_detail = (req, res) => {
 
 // Display user create form on GET
 exports.user_create_get = (req, res) => {
-    res.send('Not Implemented: User create GET');
+    res.render('user_form', {title: 'Create User'});
 }
 
 // Handle user create on POST
-exports.user_create_post = (req, res) => {
-    res.send('Not Implemented: User create POST');
-}
+exports.user_create_post = [
+    // Validate that the name field is not empty.
+    body('name', 'User name required').isLength({min:5}).trim(),
+    body('vendorcode', 'Vendor Code required').isLength({min:1}).trim(),
+    
+    
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('*').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+            // Extract the validation errors from a request.
+            const errors = validationResult(req);
+
+            // Create a genre object with escaped and trimmed data.
+            const user = new User(
+                { 
+                    name: req.body.name,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    address: req.body.address,
+                    vendorCode: req.body.vendorcode }
+            );
+
+            if (!errors.isEmpty()) {
+                // There are errors. Render the form again with sanitized values/error messages.
+                res.render('user_form', { title: 'Create User', user: user, errors: errors.array()});
+            return;
+            }
+            else {
+                // Data from form is valid.
+                // Check if Category with same name already exists.
+                User.findOne({'vendorCode': req.body.vendorcode})
+                .exec((err, found_user) => {
+                    if(err){return next(err);}
+                    if(found_user){
+                        // Category exists, redirect to its detail page.
+                        res.redirect('/dashboard/' + found_user.url)
+                    }
+                    else {
+                        user.save((err) => {
+                            if(err){return next(err);}
+                            // Category saved. Redirect to category detail page. 
+                            res.redirect('/dashboard/' + user.url);
+                        })    
+                    }
+                })
+            }
+    }
+]
 
 // Display user update form on GET
 exports.user_update_get = (req, res) => {
-    res.send('Not Implemented: User Update GET');
+    User.findById(req.params.id, (err, user) => {
+        if(err){return next(err);}
+        res.render('user_form', {title: 'Update User', user: user});  
+    });
 }
 
 // Handle user update form on POST
-exports.user_update_post = (req, res) => {
-    res.send('Not Implemented: User Update POST');
-}
+exports.user_update_post = [
+    // Validate that the name field is not empty.
+    body('name', 'User name required').isLength({min:5}).trim(),
+    body('vendorcode', 'Vendor Code required').isLength({min:1}).trim(),
+    
+    
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('*').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+            // Extract the validation errors from a request.
+            const errors = validationResult(req);
+
+            // Create a genre object with escaped and trimmed data.
+            const user = new User(
+                { 
+                    name: req.body.name,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    address: req.body.address,
+                    vendorCode: req.body.vendorcode,
+                    _id: req.params.id
+                }
+            );
+
+            if (!errors.isEmpty()) {
+                // There are errors. Render the form again with sanitized values/error messages.
+                res.render('user_form', { title: 'Update User', user: user, errors: errors.array()});
+            return;
+            }
+            else {
+                // Data from form is valid.
+                // Check if Category with same name already exists.
+                User.findByIdAndUpdate(req.params.id, user, (err) => {
+                    if(err){return next(err)}
+                    res.redirect('/dashboard/' + user.url);
+                });
+
+            }
+    }
+]
 
 // Display user delete form on GET
 exports.user_delete_get = (req, res) => {
-    res.send('Not Implemented: User Delete GET');
+    async.parallel({
+        user: (callback) => {
+            User.findById(req.params.id).exec(callback);
+        },
+        user_orders: (callback) => {
+            Order.find({'user': req.params.id}).exec(callback);
+        } 
+    }, (err, results) => {
+        if(err){return next(err);}
+        if(results.user == null){res.redirect('/dashboard/users')}
+
+        res.render('user_delete', {title: 'Delete User', user: results.user, user_orders: results.user_orders});
+    })
 }
 
 // Handle user delete form on POST
 exports.user_delete_post = (req, res) => {
-    res.send('Not Implemented: User Delete POST');
+    async.parallel({
+        user: (callback) => {
+            User.findById(req.params.id).exec(callback);
+        },
+        user_orders: (callback) => {
+            Order.find({'user': req.params.id}).exec(callback);
+        } 
+    }, (err, results) => {
+        if(err){return next(err);}
+        if(results.user_orders.length > 0){
+            res.render('user_delete', {title: 'Delete User', user: results.user, user_orders: results.user_orders});
+            return;
+        }
+        else {
+            User.findByIdAndRemove(req.body.userid, (err) => {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/dashboard/users')
+            })
+        }
+    })
 }
